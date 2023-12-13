d6b9e80e25a5624fe1d0683da9ad2edfb11fdb97
Abrantepah <christianidanfrowne@gmail.com>
2023-12-13 23:38:22 +0000

    'verifiedapi2'

--- base/api_urls.py
+++ base/api_urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from .api_views import get_students, get_lecturers, StudentLoginAPIView, LecturerLoginAPIView, student_home, permission_api, verification_api, MarkAttendance
 
 urlpatterns = [
-    path('api/students/', get_students, name='get_students'),
-    path('api/lecturers/', get_lecturers, name='get_lecturers'),
-    path('api/student-login/', StudentLoginAPIView.as_view(), name='student_login'),
-    path('api/lecturer-login/', LecturerLoginAPIView.as_view(),
-         name='lecturer_login'),
+    path('api/students/', get_students),
+    path('api/lecturers/', get_lecturers),
+    path('api/student-login/', StudentLoginAPIView.as_view()),
+    path('api/lecturer-login/', LecturerLoginAPIView.as_view()),
     path('api/student-home/<int:user_id>/', student_home, name='student_home'),
     path('api/permission/<int:user_id>/', permission_api),
     path('api/permission/<int:user_id>/<int:course_id>/',
