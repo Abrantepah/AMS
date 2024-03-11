f280b8def867a5d5fc028bc58913b49c270ff4f6
Abrantepah <christianidanfrowne@gmail.com>
2024-03-11 19:21:57 +0000

    'firsttoPostgres'

--- ams/settings.py
+++ ams/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'ams.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'VglGOtZojhpBlyDikwroAyFuLGoCHytG',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '56284',
     }
 }
 
