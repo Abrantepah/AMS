84bddb70fbc9248b1959653ef4fb6fd2f3557fb5
AhianyoElisha <elisha.ahianyo247@gmail.com>
2024-05-12 20:47:28 +0000

    removed unwanted imports

--- front-end-student/src/pages/dashboard/studentDashboard.tsx
+++ front-end-student/src/pages/dashboard/studentDashboard.tsx
@@ -1,22 +1,13 @@
-import { Row, Col, theme, Typography } from "antd";
+import { theme } from "antd";
 import { useTranslation } from "react-i18next";
-import {
-  CardWithPlot,
-  RecentCourses,
-  CardWithContent,
-} from "../../components";
-import {
-  CheckCircleOutlined,
-  ShoppingOutlined,
-} from "@ant-design/icons";
+
 import { CreateButton, List } from "@refinedev/antd";
-import { useApiUrl, useGetIdentity, useGo, useNavigation, useShow } from "@refinedev/core";
+import { useGetIdentity, useGo, useNavigation } from "@refinedev/core";
 import { useLocation } from "react-router-dom";
 import { IIdentity} from "../../interfaces";
 
 
 export const StudentDashboardPage = () => {
-  const { token } = theme.useToken();
   const { t } = useTranslation();
   const { data: user } = useGetIdentity<IIdentity>();
   const go = useGo();
@@ -60,130 +51,7 @@ export const StudentDashboardPage = () => {
         </CreateButton>
       )}
     >
-      {/* <Row gutter={[16, 32]}>
-        <Col md={24}>
-          <Row gutter={[30, 30]}>
-            <Col xl={{ span: 8 }} lg={10} md={10} sm={24} xs={24}>
-              <CardWithPlot
-                icon={
-                  <CheckCircleOutlined
-                    style={{
-                      fontSize: 14,
-                      color: token.colorPrimary,
-                    }} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
-                }
-                bodyStyles={{ textAlign: "right", paddingRight: "30px" }}
-                title={t("Active Courses")}
-                handleClick={() => {
-                  return go({
-                    to: `${listUrl("courses")}`,
-                    query: {
-                      to: pathname,
-                    },
-                    options: {
-                      keepQuery: true,
-                    },
-                    type: "replace",
-                  });
-                }}
-              >
-                <Typography
-                  style={{
-                    fontSize: 50,
-                    fontWeight: 700
-                  }}
-                >
-                  
-                  {
-                    // @ts-ignore
-                    courses.length}
-                </Typography>
-              </CardWithPlot>
-            </Col>
-            <Col xl={{ span: 8 }} lg={10} md={10} sm={24} xs={24}>
-              <CardWithPlot
-                icon={
-                  <CheckCircleOutlined
-                    style={{
-                      fontSize: 14,
-                      color: token.colorPrimary,
-                    }} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                  />
-                }
-                bodyStyles={{textAlign: "right", paddingRight: "30px"}}
-                title={t("Total Sessions Recorded")}
-                handleClick={() => {
-                  return go({
-                    to: `${listUrl("courses")}`,
-                    query: {
-                      to: pathname,
-                    },
-                    options: {
-                      keepQuery: true,
-                    },
-                    type: "replace",
-                  });
-                }}
-              >
-                <Typography
-                  style={{
-                    fontSize: 50,
-                    fontWeight: 700
-                  }}
-                >14</Typography>
-              </CardWithPlot>
-            </Col>
-            <Col xl={{ span: 8 }} lg={10} md={10} sm={24} xs={24}>
-              <CardWithPlot
-                icon={
-                  <CheckCircleOutlined
-                    style={{
-                      fontSize: 14,
-                      color: token.colorPrimary,
-                    }} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                  />
-                }
-                title={t("Completed Courses")}
-                bodyStyles={{textAlign: "right", paddingRight: "30px"}}
-                handleClick={() => {
-                  return go({
-                    to: `${listUrl("courses")}`,
-                    query: {
-                      to: pathname,
-                    },
-                    options: {
-                      keepQuery: true,
-                    },
-                    type: "replace",
-                  });
-                }}
-              >
-                <Typography
-                  style={{
-                    fontSize: 50,
-                    fontWeight: 700
-                  }}
-                >0</Typography>
-              </CardWithPlot>
-            </Col>
-          </Row>
-        </Col>
-        <Col xl={24} lg={24} md={24} sm={24} xs={24}>
-          <CardWithContent
-            bodyStyles={{
-              padding: "1px 0px 0px 0px",
-            }}
-            icon={
-              <ShoppingOutlined
-                style={{
-                  fontSize: 14,
-                  color: token.colorPrimary,
-                }} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}              />
-            }
-            title={t("Recently Accessed Courses")}
-          >
-            <RecentCourses />
-          </CardWithContent>
-        </Col>
-      </Row> */}
+
     </List>
   );
 };
