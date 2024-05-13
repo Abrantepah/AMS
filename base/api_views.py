027c7f85826a2478b70f1b6f5a187bc7546c9fe4
Abrantepah <christianidanfrowne@gmail.com>
2024-05-13 13:51:19 +0000

    'attendance'

--- base/api_views.py
+++ base/api_views.py
@@ -354,7 +354,8 @@ def MarkAttendance(request, user_id, code):
                     attendance.attended_start = True
                     attendance.save()
                     return Response({'Start atttendance Marked Successfully'}, status=status.HTTP_201_CREATED)
-
+            
+            elif attendance_type == 'end':
                 studentcode, created = StudentCode.objects.get_or_create(
                     student=student,
                     code=code,
@@ -704,6 +705,174 @@ def studentsTable(request, user_id, class_id, course_id):
     }
 
     return Response(response_data, status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+@login_required(login_url='/')
+def StudentsTable(request):
+    # Get the lecturer associated with the current user
+    lecturer = Lecturer.objects.get(user=request.user)
+
+    # Get all courses related to the lecturer
+    lecturer_courses = Course.objects.filter(lecturer=lecturer)
+
+    if lecturer_courses.exists():
+        default_course_id = lecturer_courses.first().id
+        default_course = Course.objects.get(id=default_course_id)
+        display = 'Course: ' + default_course.name + \
+            ' (Year:' + str(default_course.year) + ')'
+
+    else:
+        # Handle the case where no courses are associated with the lecturer
+        default_course = None
+        display = ''
+
+    sessions = Session.objects.filter(course=default_course).annotate(
+        student_session_modulo=((F('id') - 1) % 15) + 1
+    )
+
+    # Get all students enrolled in the courses related to the lecturer
+    students = Student.objects.filter(
+        studentcourse__course=default_course)
+
+    departments_with_lecturer = Department.objects.filter(
+        lecturer=lecturer)
+
+    if request.method == 'POST':
+        # Use request.POST to retrieve form data from a POST request
+        indexF = request.POST.get('indexFilter')
+        courseF = request.POST.get('courseFilter')
+        programmeF = request.POST.get('programmeFilter')
+        yearF = request.POST.get('yearFilter')
+        strikeF = request.POST.get('strikesFilter')
+        nameF = request.POST.get('nameFilter')
+
+    # Apply filters based on user input
+        if indexF:
+            students = Student.objects.filter(
+                index__icontains=indexF,  studentcourse__course__lecturer=lecturer)
+            display = 'Index Number: ' + indexF
+        if courseF:
+            students = Student.objects.filter(
+                studentcourse__course__name=courseF, studentcourse__course__lecturer=lecturer)
+            default_course = Course.objects.get(name=courseF)
+            display = 'Course: ' + default_course.name + \
+                ' (Year:' + str(default_course.year) + ')'
+
+        if programmeF:
+            students = Student.objects.filter(
+                programme=programmeF, studentcourse__course__lecturer=lecturer)
+            programmeName = Department.objects.get(id=programmeF)
+            display = 'Programme: ' + str(programmeName.dname)
+        if yearF:
+            students = Student.objects.filter(
+                year=yearF, studentcourse__course__lecturer=lecturer)
+            display = 'Year: ' + yearF
+        if strikeF:
+            if strikeF == '4':
+                students = Student.objects.filter(studentcourse__strike__range=[
+                    4, 15], studentcourse__course__lecturer=lecturer)
+            else:
+                students = Student.objects.filter(studentcourse__strike=int(
+                    strikeF), studentcourse__course__lecturer=lecturer)
+            display = 'Number of Absences: ' + strikeF
+        if nameF:
+            students = Student.objects.filter(
+                name__icontains=nameF, studentcourse__course__lecturer=lecturer)
+            display = 'Name: ' + nameF
+    else:
+        students = Student.objects.filter(
+            studentcourse__course=default_course)
+        display = 'Course: ' + default_course.name + \
+            ' (Year:' + str(default_course.year) + ')'
+
+    Tsessions = Session.objects.filter(
+        attendance__attended=True, course=default_course)
+
+    func_values = [(session.id - 1) % 15 + 1 for session in Tsessions]
+
+    # Prepare a dictionary to store student information and strikes
+    student_info = {}
+
+    for student in students:
+        try:
+            studentcourse = StudentCourse.objects.get(
+                student=student, course__lecturer=lecturer, course__year=student.year)
+        except:
+            message = 'nothing'
+
+        studses = StudentSession.objects.filter(studentcourse=studentcourse)
+
+        # for marking none attended sessions
+        for func in func_values:
+            for studse in studses:
+                var = ((studse.id - 1) % 15) + 1
+                if var == func:
+                    mark = studse.attended
+                    if mark is None:
+                        mark = False
+                    if mark is False:
+                        mark = False
+                    else:
+                        mark = True
+                    studse.attended = mark  # Set the value explicitly
+                    studse.save()
+
+        # Initialize the check variable inside the loop for each student
+        check = 0
+
+        for studse in studses:
+            attended = studse.attended
+            if attended is False:
+                check += 1
+
+        try:
+            studentcourse.strike = check
+            studentcourse.save()
+        except StudentCourse.DoesNotExist:
+            # Handle the case where StudentCourse does not exist for the student
+            pass
+
+        # Get the StudentCourse objects for the student and courses combination
+        student_courses = StudentCourse.objects.filter(
+            student=student, course__in=lecturer_courses)
+
+        # Store student information and strikes in the dictionary
+        student_info[student] = {
+            'student': student,
+            'student_courses': []
+        }
+
+        for student_course in student_courses:
+            # Get the strikes for that StudentCourse object
+            strikes = student_course.strike
+
+            # Get the individual student sessions for this student and course combination
+            studentsessions = StudentSession.objects.filter(
+                studentcourse=student_course)
+
+            student_info[student]['student_courses'].append({
+                'course': student_course.course,
+                'strikes': strikes,
+                'studentsessions': studentsessions,
+            })
+
+    context = {'student_info': student_info,
+               'sessions': sessions,
+               'lecturer_courses': lecturer_courses,
+               'departments': departments_with_lecturer,
+               'displayFilter': display
+               }
+    return render(request, 'base/Students.html', context)
+
+
+
+
+
 
 
 
